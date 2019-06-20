ba290569796ca283a7f4b8af99422044387e73e5
karol-piotrowski <kp0@o2.pl>
2019-06-20 18:27:58 +0200

    Project structure created

---
+++ src/main/java/com/kodilla/invoicestore/InvoicestoreApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.invoicestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/invoicestore/InvoicestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.invoicestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
